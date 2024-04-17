714b6ad58edb11c81a60cadb53b2bb097c8a15e3
DavidT59 <158860047+DavidT59@users.noreply.github.com>
2024-04-17 12:48:07 -0500

    Update delete_task.c

--- backend/delete_task.c
+++ backend/delete_task.c
@@ -1,2 +1,24 @@
-// this section works on deleting task on the  todo list
-// please read the documentation section for more information
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void deleteTask(Node *head, const char *date, const char *time, const char *description){
+    
+    Node *temp = *headNode, *prevNode = NULL;
+    
+    while (temp != NULL){
+        if(strcmp(temp->taskData.date, date) == 0 && strcmp(temp->taskData.time, time) == 0 && strcmp(temp->taskData.description, description) == 0){ //to compare the wanted task to be deleted and the task to see if correct one
+            if(prev = NULL){
+                *headNode = temp->next; // Remove the node and updates the headNote pointer
+            } else{
+                prevNode->next = temp->next;
+            }
+            free(temp);
+            return;
+        }
+        prevNode = temp;
+        temp = temp->next
+        
+    }
+    
+}
